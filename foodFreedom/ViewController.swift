d1545dc6d47fdd7703beacbac83ba8fcdd6e77b8
veerender <veerender@cloudsolv.in>
2020-12-28 20:20:56 +0530

    first commit

---
+++ foodFreedom/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  foodFreedom
+//
+//  Created by apple on 23/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func hello(_ sender: Any) {
+        
+        
+        print("hello world")
+        
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
